6fb75e6dc250dbcab66aed7cbca9949b3e42d3fa
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-06-14 11:59:44 +0000

    final work

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+
+const button = document.getElementById('view-btn')
+button.onclick = function () { 
+alert('View Available Puppies Here!')
+}
+
+const butt = document.getElementById('view-btnn')
+butt.onclick = function () { 
+alert('Join Our Guardian Program!')
+}
+
+const buttn = document.getElementById('about-btn')
+buttn.onclick = function () { 
+alert('More About Us!')
+}
+
+const btt = document.getElementById('build-btn')
+btt.onclick = function () { 
+alert('View Our Adoption Calendar Here')
+}
+
+const bttn = document.getElementById('pet-btn1')
+bttn.onclick = function () { 
+alert('PONCHO Says hi!')
+}
+
+const bt = document.getElementById('pet-btn2')
+bt.onclick = function () { 
+alert('SOURDOUGH cant wait to meet you!')
+}
+
+const apply = document.getElementById('want-btn')
+apply.onclick = function () { 
+alert('Apply Today!')
+}
+
+const bn = document.getElementById('pet-btn3')
+bn.onclick = function () { 
+alert('CERSI Says hi!')
+}
+
+const bun = document.getElementById('pet-btn4')
+bun.onclick = function () { 
+alert('ACE Says hi!')
+}
+
+const app = document.getElementById('pet-btn5')
+app.onclick = function () { 
+alert('GRAYSON cant wait to meet you!')
+}
+
+const appl = document.getElementById('pet-btn6')
+appl.onclick = function () { 
+alert('LEAPER cant wait to meet you!')
+}
